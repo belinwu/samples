517a4764da180eb65c71bb27b07a947bbde99c5a
Belin Wu <belinwu@qq.com>
2018-10-23 20:15:43 +0800

    Migrate to Gradle Kotlin DSL.

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+        maven { url = uri("http://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { url = uri("http://dl.bintray.com/kotlin/kotlin-dev") }
+        maven { url = uri("http://dl.bintray.com/kotlin/kotlinx") }
+        maven { url = uri("https://dl.bintray.com/russhwolf/multiplatform-settings") }
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:${Vers.androidGradlePlugin}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+        maven { url = uri("http://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { url = uri("http://dl.bintray.com/kotlin/kotlin-dev") }
+        maven { url = uri("http://dl.bintray.com/kotlin/kotlinx") }
+        maven { url = uri("https://dl.bintray.com/russhwolf/multiplatform-settings") }
+    }
+}
---
+++ viewmodel/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Vers.compileSdk)
+    defaultConfig {
+        applicationId = "com.samelody.samples.viewmodel"
+        minSdkVersion(Vers.minSdk)
+        targetSdkVersion(Vers.targetSdk)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Deps.kotlin)
+    implementation(Deps.appcompat)
+    implementation(Deps.room)
+    implementation(Deps.paging)
+    implementation(Deps.paging_ktx)
+    implementation(Deps.material)
+    implementation(Deps.coroutines_android)
+    implementation(Deps.lifecycle)
+    implementation(Deps.constraint)
+    testImplementation(Deps.junit)
+}
